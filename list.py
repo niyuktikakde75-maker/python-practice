75cba86281f76ab19bdff155edcbef208d8a87a7
Niyukti Kakde <niyuktikakde75@gmail.com>
2026-07-29 22:23:11 +0530

    second commit

--- list.py
+++ list.py
@@ -4,3 +4,10 @@ number = [10,20,30,40]
 names =["niyu","neha","nanidini"]
 print(number)
 print(names)
+
+
+#accessing list elements
+marks =[70,80,90,85]
+print(marks[0])
+print(marks[0:3])
+print(marks[-1])
