b02bc60308db5a10a440c2ef41fdf5ec8b37b68d
Niyukti Kakde <niyuktikakde75@gmail.com>
2026-07-29 22:19:16 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,6 @@
+#meaning of list
+#A list is a collection data type used to store multiple values in a single variable
+number = [10,20,30,40]
+names =["niyu","neha","nanidini"]
+print(number)
+print(names)
