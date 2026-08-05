8e2d526439fb5bc81d8832fdf391cbc2ea7022d0
Niyukti Kakde <niyuktikakde75@gmail.com>
2026-08-05 22:57:33 +0530

    complete question2

--- homework.py
+++ homework.py
@@ -1,6 +1,18 @@
 #1
 #create a class named student and create one object
-class student:
-    pass
-student1=student()
-print("student object created successfully")
+# class student:
+#     pass
+# student1=student()
+# print("student object created successfully")
+
+#2
+#create a stident class with name and age attributes
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print("student name :",self.name)
+        print("student age :",self.age)
+student1 = Student ("niyukti",20)
+student1.display()           
