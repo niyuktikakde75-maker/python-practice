1cbc2b280090832f3d9df477ab79beb5de066848
Niyukti Kakde <niyuktikakde75@gmail.com>
2026-08-05 22:47:52 +0530

    complete question1

---
+++ homework.py
@@ -0,0 +1,6 @@
+#1
+#create a class named student and create one object
+class student:
+    pass
+student1=student()
+print("student object created successfully")
---
+++ practice .py
@@ -0,0 +1,3 @@
+#create list of 5 students names and print all names
+students = ["niyukti","sharvari","neha","megha","nandini"]
+print(students)
