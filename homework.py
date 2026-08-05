80f67accdfbf4b0dd5db27c8bdc09730fe6b86d8
Niyukti Kakde <niyuktikakde75@gmail.com>
2026-08-05 23:11:40 +0530

    complete question3

--- homework.py
+++ homework.py
@@ -7,12 +7,31 @@
 
 #2
 #create a stident class with name and age attributes
-class Student:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+# class Student:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def display(self):
+#         print("student name :",self.name)
+#         print("student age :",self.age)
+# student1 = Student ("niyukti",20)
+# student1.display()    
+# 
+
+#3
+# create a car class with brand,model and price
+class Car:
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self .price = price     
     def display(self):
-        print("student name :",self.name)
-        print("student age :",self.age)
-student1 = Student ("niyukti",20)
-student1.display()           
+        print("Brand:", self.brand)  
+        print("Model:", self.model)
+        print("Price:",self.price)
+car1 = Car("TATA","Nexon",900000)
+car2 = Car("Hyundai","Creta",1200000)
+
+car1.display()
+print()
+car2.display()
